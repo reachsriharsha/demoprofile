1152e2f989a5db53e4d7c0d0f21c10187edd8c6d
RAKESH B R <39492020+rakeshrocky58008@users.noreply.github.com>
2025-09-05 09:31:41 +0530

    Disable file logging in demo_app.py
    Comment out the FileHandler for logging.

--- demo_app.py
+++ demo_app.py
@@ -22,7 +22,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler('gradio_app.log'),
+   #     logging.FileHandler('gradio_app.log'),
         logging.StreamHandler()  # Also log to console
     ]
 )
